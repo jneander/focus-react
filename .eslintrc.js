0f25ee9a8f693cf10c2418a48028ada10ec0cf67
Jeremy Neander <jeremy.neander@gmail.com>
2023-02-16 08:14:38 -0600

    chore: import eslint and babel configs from dev-lint library

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,75 @@
-const {configureEslint} = require('@jneander/dev-lint')
-
-module.exports = configureEslint({
-  browser: true,
-  react: true,
-})
-
-module.exports.overrides.push({
+module.exports = {
   env: {
-    node: true,
+    browser: true,
+    es6: true,
+    mocha: true,
+    node: false,
   },
 
-  files: ['./config/**/*.js', './scripts/**/*.js'],
-})
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:eslint-comments/recommended',
+    'plugin:promise/recommended',
+    'prettier/react',
+    'plugin:react/recommended',
+  ],
+
+  globals: {},
+
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+
+      files: ['./config/**/*.js', './scripts/**/*.js', './babel.config.js', './.eslintrc.js'],
+    },
+
+    {
+      files: ['./**/*.spec.js', './**/_specs_/**/*.js'],
+
+      globals: {
+        expect: 'writable',
+      },
+
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+
+  parser: '@babel/eslint-parser',
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+
+  plugins: ['import', 'prettier', 'promise', 'mocha', 'jsx-a11y', 'react', 'react-hooks'],
+  root: true,
+
+  rules: {
+    'arrow-body-style': 'off',
+    'eslint-comments/no-unused-disable': 'error',
+    'import/extensions': ['error', 'ignorePackages', {js: 'never'}],
+    'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
+    'no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    'prefer-arrow-callback': 'off',
+    'prettier/prettier': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-filename-extension': ['error', {extensions: ['.js']}],
+    'react/prop-types': 'off',
+  },
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
